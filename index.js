d02ce236fe890c4d8e33adfd330c3e58868ccd1e
Lionel Mason <capitalcookhouse@gmail.com>
2021-10-24 21:03:30 -0400

    commit tree 89eb4fc3dbde3ea034a8cd4e69acdf2670debfab

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const num1 = 31;
 const num2 = 2;
 const multiply = num1*num2;
 const random = 1;
-Number.isInterger(random)
+// Number.isInterger(random)
 const num3 = 24;
 const num4 = 5;
-const mod = num3/num4 
+const mod = num3 % num4 
+const max = 20
+Math.max(20);
 
 
 
